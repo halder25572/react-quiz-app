4fb64cd58222b2d65ee6986f3761276dac1c18e8
unknown <halder25572@gmail.com.com>
2023-02-28 00:41:10 +0600

    css color

--- src/components/QuizAns/QuizAns.js
+++ src/components/QuizAns/QuizAns.js
@@ -10,10 +10,10 @@ const QuizAns = ({quizAns}) => {
         <div className='mt-4 py-4'>
             <div className="customWidth container mx-auto p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <div className="correctBtn float-right">
-                <FontAwesomeIcon className='cursor-pointer' icon={faEye}></FontAwesomeIcon>
+                <FontAwesomeIcon className='cursor-pointer ml-4' icon={faEye}></FontAwesomeIcon>
             </div>
-                <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{question}</h5>
-                    <div className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400 grid grid-rows-2 grid-flow-col gap-2">
+                <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white text-blue-600">{question}</h5>
+                    <div className="text-blue-600 mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400 grid grid-rows-2 grid-flow-col gap-2">
                         <p className='border py-8 m-4 p-8 rounded'><input className='mr-8' type="radio" name='radio'/>{options[0]}</p>
                         <p className='border py-8 m-4 rounded'><input className='mr-8' type="radio" name='radio' />{options[1]}</p>
                         <p className='border py-8 m-4 rounded'><input className='mr-8' type="radio" name='radio' />{options[2]}</p>
