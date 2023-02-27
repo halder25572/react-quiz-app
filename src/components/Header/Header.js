778e54ee569e1dc98ce50ecadfbec19f873e65f1
unknown <halder25572@gmail.com.com>
2023-02-28 00:25:18 +0600

    css update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,35 +7,35 @@ const Header = () => {
             <header className ="bg-green-400 bg-opacity-75 text-white shadow-lg hidden md:block">
                 <div className ="container mx-auto flex items-center h-24">
                     <Link to="" className ="flex items-center justify-center">
-                    <img className ="h-16" src="https://team-creative.net/static/media/teamcreative.a00ef5cc.png" alt="" />
-                    <span className ="ml-4 uppercase font-black"><br/></span>
+                        <h1 className='text-5xl'>QuizTest</h1>
+                        <span className ="ml-4 uppercase font-black"><br/></span>
                     </Link>
                     <nav className="contents font-semibold text-base lg:text-lg">
                     <ul className="mx-auto flex items-center">
                         <li className ="p-5 xl:p-8 active">
-                        <Link to="/">
-                            <span>Home</span>
-                        </Link>
+                            <Link to="/">
+                                <span>Home</span>
+                            </Link>
                         </li>
                         <li className ="p-5 xl:p-8">
-                        <Link to="quiz">
-                            <span>Quiz</span>
-                        </Link>
+                            <Link to="quiz">
+                                <span>Quiz</span>
+                            </Link>
                         </li>
                         <li className="p-5 xl:p-8">
-                        <Link to="projects">
-                            <span>Projects</span>
-                        </Link>
+                            <Link to="projects">
+                                <span>Projects</span>
+                            </Link>
                         </li>
                         <li className="p-5 xl:p-8">
-                        <Link to="/tutorials">
-                            <span>Tutorials</span>
-                        </Link>
+                            <Link to="/tutorials">
+                                <span>Tutorials</span>
+                            </Link>
                         </li>
                         <li className ="p-5 xl:p-8">
-                        <Link to="/answer">
-                            <span>Answer</span>
-                        </Link>
+                            <Link to="/answer">
+                                <span>Answer</span>
+                            </Link>
                         </li>
                     </ul>
                     </nav>
--- src/components/QuizAns/QuizAns.js
+++ src/components/QuizAns/QuizAns.js
@@ -1,27 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import {faEye} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const QuizAns = ({quizAns}) => {
     const {id, correctAnswer, question, options} = quizAns;
     return (
         <div className='mt-4 py-4'>
-            {/* <div className='question'>
-                <h2 className='text-3xl'>{question}</h2>
+            <div className="customWidth container mx-auto p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="correctBtn float-right">
+                <FontAwesomeIcon className='cursor-pointer' icon={faEye}></FontAwesomeIcon>
             </div>
-            <div className='grid grid-cols-2'>
-                <p className=''><input type="radio" name='radio'/>{options[0]}</p>
-                <p className=''><input type="radio" name='radio' />{options[1]}</p>
-                <p className=''><input type="radio" name='radio' />{options[2]}</p>
-                <p className=''><input type="radio" name='radio' />{options[3]}</p>
-            </div> */}
-            
-            <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{question}</h5>
-                    <div className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-                        <p className='border py-8 m-4 p-8'><input className='mr-8' type="radio" name='radio'/>{options[0]}</p>
-                        <p className='border py-8 m-4'><input className='mr-8' type="radio" name='radio' />{options[1]}</p>
-                        <p className='border py-8 m-4'><input className='mr-8' type="radio" name='radio' />{options[2]}</p>
-                        <p className='border py-8 m-4'><input className='mr-8' type="radio" name='radio' />{options[3]}</p>
+                    <div className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400 grid grid-rows-2 grid-flow-col gap-2">
+                        <p className='border py-8 m-4 p-8 rounded'><input className='mr-8' type="radio" name='radio'/>{options[0]}</p>
+                        <p className='border py-8 m-4 rounded'><input className='mr-8' type="radio" name='radio' />{options[1]}</p>
+                        <p className='border py-8 m-4 rounded'><input className='mr-8' type="radio" name='radio' />{options[2]}</p>
+                        <p className='border py-8 m-4 rounded'><input className='mr-8' type="radio" name='radio' />{options[3]}</p>
                     </div>
             </div>
 
